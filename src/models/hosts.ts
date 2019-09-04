42598390c00a16ca486af175b2316220b269156b
codermarcos <coder.marcos@gmail.com>
2019-09-04 16:43:10 -0300

    :new: Create api interfaces

---
+++ src/models/hosts.ts
@@ -0,0 +1,41 @@
+
+
+export interface Highlight {
+  name: string;
+  value: string;
+}
+
+export interface LastReview {
+  body: string;
+  client_image_url: string;
+  client_name: string;
+}
+
+export interface Locale {
+  country: string;
+  price: number;
+  currency: string;
+}
+
+export interface User {
+  first_name: string;
+  image_url: string;
+}
+
+export interface Host {
+  availability_confirmed: boolean;
+  chopped_desc: string;
+  highlights: Highlight[];
+  favorite: boolean;
+  id: number;
+  is_superhero: boolean;
+  last_review: LastReview;
+  locale: Locale;
+  rating: number;
+  region_address: string;
+  reviews_count: number;
+  title: string;
+  user: User;
+}
+
+export interface Hosts extends Array<Host> {}
