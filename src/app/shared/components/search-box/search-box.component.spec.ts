375ff9e76ffa7ebac2299495bb730c77436b0349
codermarcos <coder.marcos@gmail.com>
2019-09-05 11:20:08 -0300

    :new: Add form search heroes

--- src/app/shared/components/search-box/search-box.component.spec.ts
+++ src/app/shared/components/search-box/search-box.component.spec.ts
@@ -1,25 +1,48 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SearchBoxComponent } from './search-box.component';
+import { SearchBoxComponent } from 'src/app/shared/components/search-box/search-box.component';
 
-describe('SearchBoxComponent', () => {
-  let component: SearchBoxComponent;
-  let fixture: ComponentFixture<SearchBoxComponent>;
+describe(
+  'SearchBoxComponent',
+  () => {
+    let component: SearchBoxComponent;
+    let fixture: ComponentFixture<SearchBoxComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchBoxComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(
+      async(
+        () => {
+          @Pipe({ name: 'translate' })
+          class TranslateMock implements PipeTransform {
+            transform(): string {
+              return 'translated';
+            }
+          }
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+          TestBed
+            .configureTestingModule({
+              declarations: [
+                SearchBoxComponent,
+                TranslateMock,
+              ],
+              imports: [
+                ReactiveFormsModule,
+                FormsModule,
+              ]
+            })
+            .compileComponents();
+        }
+      )
+    );
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SearchBoxComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'form[appSearchBox]',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent {
+
+  private today = new Date();
+
+  @Output() submit = new EventEmitter();
+
+  public startDate = new FormControl(this.today.toJSON().split('T')[0]);
+  public endDate = new FormControl(this.today.toJSON().split('T')[0]);
+  public address = new FormControl();
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  public send() {
+    const form: { [key: string]: string } = {};
 
+    this.address.value && (form.region_address = `^${this.address.value}`);
+
+    this.submit.emit(form);
+  }
 }
