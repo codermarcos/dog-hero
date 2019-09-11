bdcecca4c779cdb1217f9eb5f68c42c2f0edcf05
codermarcos <coder.marcos@gmail.com>
2019-09-10 21:06:04 -0300

    :bug: Fix error translate

--- src/app/i18n/loader.translate.ts
+++ src/app/i18n/loader.translate.ts
@@ -1,12 +1,11 @@
-import { of, Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { Provider } from '@angular/core';
-import * as pt from 'src/assets/i18n/pt.json';
 import { TranslateLoader } from '@ngx-translate/core';
 
 export const loader: Provider = {
   useClass: class implements TranslateLoader {
     getTranslation(lang: string): Observable<any> {
-      return of(pt);
+      return from(fetch(`/i18n/${lang}`));
     }
   },
   provide: TranslateLoader
