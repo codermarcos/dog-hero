ff2d7590ffd3a62fe4e17086d1026b40672e3291
codermarcos <coder.marcos@gmail.com>
2019-09-10 21:38:50 -0300

    :bug: Fix error translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import locale from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { TranslateModule } from '@ngx-translate/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 
 import { AppComponent } from 'src/app/app.component';
 
@@ -13,13 +16,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { MockService } from 'src/app/services/mock/mock.service';
 import { HeroService } from 'src/app/services/hero/hero.service';
 
+registerLocaleData(locale, 'pt');
+
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
     InMemoryWebApiModule.forRoot(MockService, { passThruUnknownUrl: true }),
-    TranslateModule.forRoot(),
+    TranslateModule.forRoot({ loader: Loader }),
     NgxPaginationModule,
     HttpClientModule,
     BrowserModule,
