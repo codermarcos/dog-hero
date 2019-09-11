7ad53e9d4587300dc15af39ba103cd625ab61ef2
codermarcos <coder.marcos@gmail.com>
2019-09-11 00:33:59 -0300

    :traffic_light: Add el to run tests

--- src/app/shared/directives/hero-highlight-text/hero-highlight-text.directive.spec.ts
+++ src/app/shared/directives/hero-highlight-text/hero-highlight-text.directive.spec.ts
@@ -1,8 +1,10 @@
-import { HeroHighlightTextDirective } from './hero-highlight-text.directive';
+import { ElementRef } from '@angular/core';
+import { HeroHighlightTextDirective } from 'src/app/shared/directives/hero-highlight-text/hero-highlight-text.directive';
 
 describe('HeroHighlightTextDirective', () => {
   it('should create an instance', () => {
-    const directive = new HeroHighlightTextDirective();
+    const el = new ElementRef(document.createElement('div'));
+    const directive = new HeroHighlightTextDirective(el);
     expect(directive).toBeTruthy();
   });
 });
